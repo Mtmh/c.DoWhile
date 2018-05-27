4ac9fd08eb44f06ca2db52e659f8e3593d82d0de
Tiziano Martin hernando <tizianomartinhernando@MacBook-Pro-de-Tiziano.local>
2018-05-27 18:19:18 +0200

    first commit

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int cuenta = 0;
+    int numero_de_veces = 3;
+
+     do {
+        printf("Hello World!\n");
+        ++cuenta;
+    }
+    while (cuenta != numero_de_veces);
+
+
+    return 0;
+}
